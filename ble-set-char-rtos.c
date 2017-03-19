4b1e6e20268f38b52749a763b03c4af0b6771928
Tom Victor <vjtomvictor@icloud.com>
2017-03-19 18:03:09 +0530

    initial code

---
+++ ble-set-char-rtos.c
@@ -0,0 +1,5 @@
+ //working code
+ SimpleProfile_SetParameter(SIMPLEPROFILE_CHAR2, sizeof(uint8_t),
+                                                 &mycount);
+                  mycount++;
+                  Display_print1(dispHandle, 4, 0, "count value : %d", mycount);
