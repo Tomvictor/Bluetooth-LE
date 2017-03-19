319504178753e40df3b6a62d7bef4a9d4a75e0e3
Tom Victor <vjtomvictor@icloud.com>
2017-03-19 18:49:57 +0530

    sensor task added, working fine

--- ble-set-char-rtos.c
+++ ble-set-char-rtos.c
@@ -3,3 +3,11 @@
                                                  &mycount);
                   mycount++;
                   Display_print1(dispHandle, 4, 0, "count value : %d", mycount);
+
+
+ // another example for notiff service
+
+   SimpleProfile_SetParameter(SIMPLEPROFILE_CHAR3, sizeof(uint8_t),
+                                                 &mycount);
+                  mycount++;
+                  Display_print1(dispHandle, 4, 0, "count value : %d", mycount);
